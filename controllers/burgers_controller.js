cb2b4915147439ce1b3d2297213e6323d5743912
Dara9234 <dahmed@alumni.depaul.edu>
2019-01-20 21:34:28 -0600

    set controller and routers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,18 +6,27 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 
+router.get('/burgers',function (req,res){
+    burgers.all(function(data){
+        var hbsObject = {burgers:data};
+        console.log(hsObject);
+        res.render('index',hbsObject);
+    });
+});
 
+router.post('/burgers/create',function(req,res){
+    burger.create(['burger_name','Tuna Burger'], [req.body.Burger_name,req.body.Tuna],function(){
+        res.redirect('/burgers');
+    });
+});
 
+router.put ('burgers/update/:id',function(req, res){
+    var condition = 'id = ' + req.params.id;
 
-
-
-
-
-
-
-
-
-
+    console.log('condition',condition);
+    res.redirect('/burgers');
+    
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+// express import
 var express = require("express");
 
 
@@ -6,22 +8,32 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(process.cwd() +"/public"));
 
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+app.use(bodyParser.urlencoded({
+  extended:false
+}));
+
+// override with POST having ?_method =DELETE
+app.use(methodOverride('_method'));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Parse request body as JSON
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burger_Controller.js");
 
 app.use(routes);
+// app.use('/', routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
